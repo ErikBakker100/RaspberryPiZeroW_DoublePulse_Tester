87e2a4b208cd17408929ffa8f66e92b47a2cb38c
Erik Bakker <e.e.bakker@online.nl>
2025-05-24 14:45:32 +0200

    added JTAG debug configuration OpenOCD

---
+++ bootloader32bit/periph.c
@@ -0,0 +1,214 @@
+
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+
+#define PBASE 0x20000000
+
+extern void PUT32 ( unsigned int, unsigned int );
+extern void PUT16 ( unsigned int, unsigned int );
+extern void PUT8 ( unsigned int, unsigned int );
+extern unsigned int GET32 ( unsigned int );
+extern void dummy ( unsigned int );
+
+#define ARM_TIMER_CTL   (PBASE+0x0000B408)
+#define ARM_TIMER_CNT   (PBASE+0x0000B420)
+
+#define GPFSEL1         (PBASE+0x00200004)
+#define GPFSEL2         (PBASE+0x00200008)
+#define GPFSEL3         (PBASE+0x0020000C)
+#define GPFSEL4         (PBASE+0x00200010)
+#define GPSET0          (PBASE+0x0020001C)
+#define GPCLR0          (PBASE+0x00200028)
+#define GPSET1          (PBASE+0x00200020)
+#define GPCLR1          (PBASE+0x2020002C)
+#define GPPUD           (PBASE+0x00200094)
+#define GPPUDCLK0       (PBASE+0x00200098)
+
+#define AUX_ENABLES     (PBASE+0x00215004)
+#define AUX_MU_IO_REG   (PBASE+0x00215040)
+#define AUX_MU_IER_REG  (PBASE+0x00215044)
+#define AUX_MU_IIR_REG  (PBASE+0x00215048)
+#define AUX_MU_LCR_REG  (PBASE+0x0021504C)
+#define AUX_MU_MCR_REG  (PBASE+0x00215050)
+#define AUX_MU_LSR_REG  (PBASE+0x00215054)
+#define AUX_MU_MSR_REG  (PBASE+0x00215058)
+#define AUX_MU_SCRATCH  (PBASE+0x0021505C)
+#define AUX_MU_CNTL_REG (PBASE+0x00215060)
+#define AUX_MU_STAT_REG (PBASE+0x00215064)
+#define AUX_MU_BAUD_REG (PBASE+0x00215068)
+
+//GPIO14  TXD0 and TXD1
+//GPIO15  RXD0 and RXD1
+
+//((250,000,000/115200)/8)-1 = 270
+//((250,000,000/19200)/8)-1 = 1626
+//2  outer corner
+//4
+//6  ground
+//8  TX out
+//10 RX in
+
+//------------------------------------------------------------------------
+unsigned int uart_lcr ( void )
+{
+    return(GET32(AUX_MU_LSR_REG));
+}
+//------------------------------------------------------------------------
+unsigned int uart_recv ( void )
+{
+    while(1)
+    {
+        if(GET32(AUX_MU_LSR_REG)&0x01) break;
+    }
+    return(GET32(AUX_MU_IO_REG)&0xFF);
+}
+//------------------------------------------------------------------------
+unsigned int uart_check ( void )
+{
+    if(GET32(AUX_MU_LSR_REG)&0x01) return(1);
+    return(0);
+}
+//------------------------------------------------------------------------
+void uart_send ( unsigned int c )
+{
+    while(1)
+    {
+        if(GET32(AUX_MU_LSR_REG)&0x20) break;
+    }
+    PUT32(AUX_MU_IO_REG,c);
+}
+//------------------------------------------------------------------------
+void uart_flush ( void )
+{
+    while(1)
+    {
+        if(GET32(AUX_MU_LSR_REG)&0x40) break;
+    }
+}
+//------------------------------------------------------------------------
+void hexstrings ( unsigned int d )
+{
+    //unsigned int ra;
+    unsigned int rb;
+    unsigned int rc;
+
+    rb=32;
+    while(1)
+    {
+        rb-=4;
+        rc=(d>>rb)&0xF;
+        if(rc>9) rc+=0x37; else rc+=0x30;
+        uart_send(rc);
+        if(rb==0) break;
+    }
+    uart_send(0x20);
+}
+//------------------------------------------------------------------------
+void hexstring ( unsigned int d )
+{
+    hexstrings(d);
+    uart_send(0x0D);
+    uart_send(0x0A);
+}
+//------------------------------------------------------------------------
+void uart_init ( void )
+{
+    unsigned int ra;
+
+    PUT32(AUX_ENABLES,1);       // Enable Mini UART
+    PUT32(AUX_MU_IER_REG,0);    // Disable interrupts
+    PUT32(AUX_MU_CNTL_REG,0);   // Disable Mini UART
+    PUT32(AUX_MU_LCR_REG,3);    // Set 8 data bits
+    PUT32(AUX_MU_MCR_REG,0);    // Disable auto flow control
+    PUT32(AUX_MU_IER_REG,0);    // Disable interrupts
+    PUT32(AUX_MU_IIR_REG,0xC6); 
+    PUT32(AUX_MU_BAUD_REG,270); // Set Baud rate to 115200
+
+    // GPIO 14 en GPIO 15 zijn de TXD0 en RXD0 pinnen van de UART.
+    ra=GET32(GPFSEL1);
+    ra&=~(7<<12); //gpio14
+    ra|=2<<12;    //alt5
+    ra&=~(7<<15); //gpio15
+    ra|=2<<15;    //alt5
+    PUT32(GPFSEL1,ra);
+
+#ifdef DEBUG // If 'make debug' used
+    #warning "DEBUG is defined"
+    // GPIO 22 t/m 27 zijn de JTAG-pinnen. Deze moeten worden ingesteld op ALT4.
+    // Eerst TRST (GPIO22) als output zetten en hoog maken
+    ra = GET32(GPFSEL2);
+    ra &= ~(7 << 6);   // clear bits for GPIO22
+    ra |= 1 << 6;      // set to output (ALT0 = 100, output = 001)
+    PUT32(GPFSEL2, ra);
+    PUT32(GPSET0, (1 << 22)); // zet GPIO22 hoog (TRST)
+    // De GPIO-pinnen voor JTAG zijn als volgt:
+    /* GPIO-pinnen voor JTAG (ALT4)
+    GPIO	JTAG-functie	ALT functie
+    GPIO 22	ARM_TRST	ALT4
+    GPIO 23	ARM_RTCK	ALT4
+    GPIO 24	ARM_TDO	ALT4
+    GPIO 25	ARM_TCK	ALT4
+    GPIO 26	ARM_TDI	ALT4
+    GPIO 27	ARM_TMS	ALT4 */
+    ra=GET32(GPFSEL2); // // GPIO 22–27 zitten in GPFSEL2
+    ra&=~(7<<6); //gpio22
+    ra|=4<<6;    //alt4
+    ra&=~(7<<9); //gpio23
+    ra|=4<<9;    //alt4
+    ra&=~(7<<12); //gpio24
+    ra|=4<<12;    //alt4
+    ra&=~(7<<15); //gpio25
+    ra|=4<<15;    //alt4
+    ra&=~(7<<18); //gpio26
+    ra|=4<<18;    //alt4
+    ra&=~(7<<21); //gpio27
+    ra|=4<<21;    //alt4
+    PUT32(GPFSEL2,ra);
+#endif
+
+    //Zonder dit kan ruis de pin beïnvloeden als de lijn zweeft.
+    //PUT32(GPPUD,0);
+    //for(ra=0;ra<150;ra++) dummy(ra);
+    //PUT32(GPPUDCLK0,(1<<14)|(1<<15));
+    //for(ra=0;ra<150;ra++) dummy(ra);
+    //PUT32(GPPUDCLK0,0);
+    PUT32(AUX_MU_CNTL_REG,3); // enable TX and RX
+}
+//------------------------------------------------------------------------
+void  timer_init ( void )
+{
+    //0xF9+1 = 250
+    //250MHz/250 = 1MHz
+    PUT32(ARM_TIMER_CTL,0x00F90000);
+    PUT32(ARM_TIMER_CTL,0x00F90200);
+}
+//-------------------------------------------------------------------------
+unsigned int timer_tick ( void )
+{
+    return(GET32(ARM_TIMER_CNT));
+}
+//-------------------------------------------------------------------------
+void leds_off ( void )
+{
+    unsigned int ra;
+
+    ra=GET32(GPFSEL4);
+    ra&=~(7<<21);
+    ra|=1<<21;
+    PUT32(GPFSEL4,ra);
+
+    PUT32(GPSET1,1<<(47-32));
+}
+//-------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------
+//
+// Copyright (c) 2012 David Welch dwelch@dwelch.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//-------------------------------------------------------------------------
